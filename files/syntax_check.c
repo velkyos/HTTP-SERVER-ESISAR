490f5e1e86fe0ce3091d545b4060c54f3454ff26
Benjamin Robert <benji.robertpro@gmail.com>
2022-03-10 00:30:48 +0100

    syntax additions
    - Adding Group Rules
    - Correcting get_abnf_rules
    - Adding String Rules

--- files/syntax_check.c
+++ files/syntax_check.c
@@ -12,9 +12,9 @@
 
 /* Constants */
 
-#define S_DEBUG 0
+#define S_DEBUG 1
 #define S_DEBUG_PATH 0
-#define S_DEBUG_TOKEN 0
+#define S_DEBUG_TOKEN 1
 #define S_OR 47
 #define S_STRING 34
 #define S_SPACE 32
@@ -42,17 +42,17 @@ char *get_end_or(const char *str, const char *str_end);
 
 char *get_end_or_group(const char *str, const char *str_end);
 
-int handle_terminal_number_rule(char c, const char *str, const char *str_end, char **next_srt);
+int handle_terminal_number_rule(char c, const char *rule, const char *rule_end, char **next_srt);
 
-int handle_terminal_string_rule(const char *request, const char *str, const char *str_end, char **next_srt);
+int handle_terminal_string_rule(const char *request, const char *rule, const char *rule_end, char **next_srt);
 
-int handle_or_rule(const char *request, derivation_tree *previous_node, const char *str, const char *str_end, char **next_srt);
+int handle_or_rule(const char *request, derivation_tree *previous_node, const char *rule, const char *rule_end, char **next_srt);
 
-int handle_repetition_rule(const char *request, derivation_tree *previous_node, const char *str, const char *str_end, char **next_srt);
+int handle_repetition_rule(const char *request, derivation_tree *previous_node, const char *rule, const char *rule_end, char **next_srt);
 
-int handle_optional_rule(const char *request, derivation_tree *previous_node, const char *str, const char *str_end, char **next_srt);
+int handle_optional_rule(const char *request, derivation_tree *previous_node, const char *rule, const char *rule_end, char **next_srt);
 
-int handle_group_rule(const char *request, derivation_tree *previous_node, const char *str, const char *str_end, char **next_srt);
+int handle_group_rule(const char *request, derivation_tree *previous_node, const char *rule, const char *rule_end, char **next_srt);
 
 /* Definition */
 
@@ -77,6 +77,8 @@ int check_for_syntax(const char *request, derivation_tree *previous_node, const 
 
 	while (reach_str_end(rule_descr, rule_end) == 0)
 	{
+		if ( S_DEBUG ) printf("RULE : %.*s\n", (rule_end-rule_descr),rule_descr);
+
 		next_pos = NULL;
 		next_token_length = -1;
 		is_valid = 0;
@@ -110,18 +112,23 @@ int check_for_syntax(const char *request, derivation_tree *previous_node, const 
 
 			}else{
 				
-				int length = get_end_rule( rule_descr, rule_end) - rule_descr;  //We Get the length of the name of the embedeed Rule.
-
+				int length = get_end_rule( rule_descr, rule_end) - rule_descr + 1;  //We Get the length of the name of the embedeed Rule.
 				abnf_rule *new_rule = get_abnf_rule( rule_descr, length); //We get the new abnf rule.
 
 				// TODO : Créer current_node  (level + 1) (Child NULL) (TAG new_rule->name) (Value -> request)
 
 				next_token_length = check_for_syntax( request, current_node, new_rule->description, NULL);
 
-				if (next_token_length != S_NOT_VALID) ;// TODO :  Add current to previous
+				
+
+				if (next_token_length != S_NOT_VALID){
+					next_pos = get_next_rule(rule_descr, rule_end);
+				} // TODO :  Add current to previous
 				else ;// TODO : Delete Tree depuis current
 			}
 		}
+
+		if ( S_DEBUG_PATH ) printf("Path -> Next_rule : %.*s\n", (rule_end-next_pos),next_pos);
 
 		is_valid = next_token_length != S_NOT_VALID;
 		
@@ -141,6 +148,8 @@ int check_for_syntax(const char *request, derivation_tree *previous_node, const 
 	else
 	{
 		/* TODO : Add value to previous */
+
+		if ( S_DEBUG_TOKEN ) printf("Token (%d)\n",token_length);
 		return token_length;
 	}
 	
@@ -150,7 +159,9 @@ abnf_rule *get_abnf_rule(const char *name, int name_length){
 	abnf_rule *temp_rules = rules;
 	while ( temp_rules->name != NULL)
 	{
-		if( strncmp( temp_rules->name, name, name_length) == 0) return temp_rules;
+		if( strncmp( temp_rules->name, name, name_length) == 0){
+			if ( strlen(temp_rules->name) == name_length) return temp_rules;
+		}
 		temp_rules++;
 	}
 	printf("Error on abnf rules !\n");
@@ -158,7 +169,7 @@ abnf_rule *get_abnf_rule(const char *name, int name_length){
 }
 
 int reach_str_end(const char *str, const char *str_end){
-	if ( str_end != NULL) return str > str_end;
+	if ( str_end != NULL && str != NULL) return str > str_end;
 	else if (str == NULL) return 1;
 	else return 0;
 }
@@ -177,16 +188,17 @@ char *get_end_rule(const char *str, const char *str_end){
 	switch ( *str)
 	{
 	case '[':
-		//TO DO : Adding [] Support
+		str = get_end_group(str + 1,str_end,'[',']');
 		break;
 	case '(':
-		//TO DO : Adding () Support
+		str = get_end_group(str + 1,str_end,'(',')');
 		break;
 	case '\"':
 		str = cs_strchr(str + 1, '\"', str_end);
 		break;
 	default:
-	    str = cs_strpbrk(str, " )]\0",str_end);
+	    str = cs_strpbrk(str, " )]",str_end);
+		if ( str != NULL) --str;
 		break;
 	}
 	return str;
@@ -194,7 +206,11 @@ char *get_end_rule(const char *str, const char *str_end){
 
 char *get_next_rule(const char *str, const char *str_end){
     char *n_str = get_end_rule(str, str_end);
-	if (n_str != NULL) n_str++;
+	if (n_str != NULL) {
+		n_str++;
+		while ( *n_str == S_SPACE) n_str++;
+		if ( *n_str == '\0' || *n_str == ']' || *n_str == ')') n_str = NULL;
+	}
 	return  n_str;
 }
 
@@ -204,13 +220,24 @@ char *get_start_rule(const char *str, const char *str_end){
 		str++;
 		while ( *str == S_SPACE) str++;
 	}
-	else if ( *str == S_STRING || *str == '%') str++;
+	else if ( *str == S_STRING) str++;
+	else if ( *str == '%') str += 2;
 
 	return reach_str_end(str, str_end) ? NULL : str;
 }
 
 char *get_end_group(const char *str, const char *str_end, char open, char close){
-	printf("Not Implemented\n");
+	int in_block_cnt = 1;
+	while (!reach_str_end( str, str_end) && *str != '\0')
+	{
+		if ( *str == open) in_block_cnt++; 
+		else if( *str == close && in_block_cnt > 0) in_block_cnt--;
+
+		if (*str == close && in_block_cnt == 0) return str; // Success
+
+		str++;
+	}
+	// Failure
 	return NULL;
 }
 
@@ -220,19 +247,19 @@ char *get_end_or(const char *str, const char *str_end){
 }
 
 char *get_end_or_group(const char *str, const char *str_end){
-	printf("Not Implemented\n");
+	//printf("Not Implemented\n");
 	return NULL;
 }
 
-int handle_terminal_number_rule(char c, const char *str, const char *str_end, char **next_srt){
+int handle_terminal_number_rule(char c, const char *rule, const char *rule_end, char **next_srt){
 
-	const char *t_str = str + 2; //Str is on % so we go to the number
+	char *s_str = get_start_rule(rule, rule_end); //Str is on % so we go to the number
 
-	char val_1 = (char)strtol(t_str, NULL, 16);
+	char val_1 = (char)strtol(s_str, NULL, 16);
 	char val_2 = val_1;
 	
-	const char *rule_dash = cs_strchr(t_str, '-', str_end);
-	const char *rule_point = cs_strchr(t_str, '.', str_end);
+	char *rule_dash = cs_strchr(s_str, '-', rule_end);
+	char *rule_point = cs_strchr(s_str, '.', rule_end);
 
 	if ( rule_dash != NULL) /* Case of %xxx-xx */
 	{
@@ -245,41 +272,66 @@ int handle_terminal_number_rule(char c, const char *str, const char *str_end, ch
 		{
 			val_1 = (char)strtol(++rule_point, NULL, 16);
 
-			rule_point = cs_strchr(rule_point, '.', str_end);
+			rule_point = cs_strchr(rule_point, '.', rule_end);
 		}
 		val_2 = val_1;
 	}
 
 	if ( c >= val_1 && c <= val_2 ){
-		*next_srt = get_next_rule( str, str_end);
+		*next_srt = get_next_rule( rule, rule_end);
 		return 1;
 	}
 	return S_NOT_VALID;
 }
 
-int handle_terminal_string_rule(const char *request, const char *str, const char *str_end, char **next_srt){
+int handle_terminal_string_rule(const char *request, const char *rule, const char *rule_end, char **next_srt){
+	char *s_str = get_start_rule(rule, rule_end);
+    char *e_str = get_end_rule(rule, rule_end) - 1;
+
+	int isSame = 1;
+	int token_length = 0;
+
+	while ( isSame && !reach_str_end(s_str,e_str))
+	{
+		if (*request != '\0') isSame = tolower(*request) == tolower(*s_str);
+		else isSame = 0;
+		token_length++;
+		request++;
+		s_str++;
+	}
+	if ( isSame )
+	{
+		*next_srt = get_next_rule(rule, rule_end);
+		return token_length;
+	}
+	return S_NOT_VALID;
+}
+
+int handle_or_rule(const char *request, derivation_tree *previous_node, const char *rule, const char *rule_end, char **next_srt){
 	printf("Not Implemented\n");
 	return 0;
 }
 
-int handle_or_rule(const char *request, derivation_tree *previous_node, const char *str, const char *str_end, char **next_srt){
+int handle_repetition_rule(const char *request, derivation_tree *previous_node, const char *rule, const char *rule_end, char **next_srt){
 	printf("Not Implemented\n");
 	return 0;
 }
 
-int handle_repetition_rule(const char *request, derivation_tree *previous_node, const char *str, const char *str_end, char **next_srt){
+int handle_optional_rule(const char *request, derivation_tree *previous_node, const char *rule, const char *rule_end, char **next_srt){
 	printf("Not Implemented\n");
 	return 0;
 }
 
-int handle_optional_rule(const char *request, derivation_tree *previous_node, const char *str, const char *str_end, char **next_srt){
-	printf("Not Implemented\n");
-	return 0;
-}
+int handle_group_rule(const char *request, derivation_tree *previous_node, const char *rule, const char *rule_end, char **next_srt){
+	char *s_str = get_start_rule(rule, rule_end);
+    char *e_str = get_end_rule(rule, rule_end);
+	int token_length = check_for_syntax(request, previous_node, s_str, e_str);
 
-int handle_group_rule(const char *request, derivation_tree *previous_node, const char *str, const char *str_end, char **next_srt){
-	printf("Not Implemented\n");
-	return 0;
+	if ( token_length != S_NOT_VALID) {
+		*next_srt = get_next_rule(rule,rule_end);
+	}
+
+	return token_length;
 }
 
 /* Abnf_rules */
@@ -310,6 +362,7 @@ static abnf_rule rules[] = {
 	{"mot","1*ALPHA separateur"},
 	{"message","debut ( mot ponct / nombre separateur ) [ ponct ] fin LF"},
 	// HTTP ABNF
-
+	{"TEST_TEXT","\"Bonjour je suis\" SP \"ça va ?\""},
+	{"TEST","SP TEST_TEXT SP (HTAB (DQUOTE))"},
 	{NULL,NULL}
 }; /**< All abnf rules*/
