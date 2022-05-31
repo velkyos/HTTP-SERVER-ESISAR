f635a091c601d96565946822c88516bede942dc5
Benjamin Robert <benji.robertpro@gmail.com>
2022-06-01 01:22:45 +0200

    Polishing config

--- src/config.c
+++ src/config.c
@@ -1,66 +1,5 @@
-/**
- * @file config.c
- * @author ROBERT Benjamin
- * @brief Process the config file.
- * @version 1.0
- * @date 2022-04-8
- * 
- * @see Config_server
- * @see Website
- * 
- * Exemple du fichier config :
- * \code{.ini}
-[CONFIG]
-port=8080  #Port d'écoute
-hosts=3    #Nombre de site
-maxcycle=0 #Nombre de fois que le serveur va répondre à une requête, 0 si infini
-timeout=5
-maxalive=15
-
-[HOST:0]
-name=toto0.com
-root=websites/toto0.com/
-index=index0.html
-
-[HOST:1]
-name=toto1.com
-root=websites/toto1.com/
-index=index2.html
-
-[HOST:2]
-name=toto2.com
-root=websites/toto2.com/
-index=index2.html
- * \endcode
- */
-
-/* System Includes */
-
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
-
-/* User Includes */
-
 #include "config.h"
-#include "utils.h"
 
-/* Declaration */
-
-int find_field_s(char *start, char *name, char *end, char **dest, int *len);
-int find_field_n(char *start, char *name, char *end, int *dest);
-char *find_section(char *start, char *name, char **end);
-
-/* Definition */
-/**
- * @brief Read the config file `file_name`, if not found return NULL
- * @see Config_server
- * @see Website
- * 
- * @param file_name 
- * @return NULL or the pointer to the struct with all the config infos
- */
 Config_server *get_config(char *file_name){
 	char *file = read_file(file_name, NULL);
 
@@ -72,12 +11,12 @@ Config_server *get_config(char *file_name){
 
     ptr = find_section(file, "[CONFIG]", &end);
 
-    if ( !find_field_n(ptr, "port=", end, &config->port ) ) return NULL;
-    if ( !find_field_n(ptr, "hosts=", end, &config->hosts ) ) return NULL;
-    if ( !find_field_n(ptr, "maxcycle=", end, &config->maxcycle ) ) return NULL;
-	if ( !find_field_n(ptr, "php_port=", end, &config->phpport ) ) return NULL;
-	if ( !find_field_n(ptr, "timeout=", end, &config->keepTimeOut ) ) return NULL;
-	if ( !find_field_n(ptr, "maxalive=", end, &config->keepMax ) ) return NULL;
+    if ( find_field_n(ptr, "port=", end, &config->port ) ) return NULL;
+    if ( find_field_n(ptr, "hosts=", end, &config->hosts ) ) return NULL;
+    if ( find_field_n(ptr, "maxcycle=", end, &config->maxcycle ) ) return NULL;
+	if ( find_field_n(ptr, "php_port=", end, &config->phpport ) ) return NULL;
+	if ( find_field_n(ptr, "timeout=", end, &config->keepTimeOut ) ) return NULL;
+	if ( find_field_n(ptr, "maxalive=", end, &config->keepMax ) ) return NULL;
 
     config->websites = malloc( sizeof(Website) * config->hosts);
 
@@ -89,11 +28,37 @@ Config_server *get_config(char *file_name){
 
         if (!(ptr)) return NULL;
 
-        if ( !find_field_s(ptr, "name=", end, &config->websites[i].name, &config->websites[i].name_len ) ) return NULL;
-        if ( !find_field_s(ptr, "root=", end, &config->websites[i].root, &config->websites[i].root_len ) ) return NULL;
-        if ( !find_field_s(ptr, "index=", end, &config->websites[i].index, &config->websites[i].index_len ) ) return NULL;
+        if ( find_field_s(ptr, "name=", end, &config->websites[i].name, &config->websites[i].name_len ) ) return NULL;
+        if ( find_field_s(ptr, "root=", end, &config->websites[i].root, &config->websites[i].root_len ) ) return NULL;
+        if ( find_field_s(ptr, "index=", end, &config->websites[i].index, &config->websites[i].index_len ) ) return NULL;
     }
     return config;
+}
+
+Website *find_website(Config_server *config, char *host, int len){
+	int count = config->hosts;
+	for (int i = 0; i < count; i++)
+	{
+		if ( strncmp(host, config->websites[i].name, len) == 0) return &config->websites[i];
+	}
+	return NULL;
+}
+
+void generate_config_file(){
+    FILE *file = open_file("server.ini","w");
+    fprintf(file,"[CONFIG]\nport=8080  #Listen Port\nphp_port=9000 #Port of the php server\nhosts=1  #Numbers of host\nmaxcycle=0   #How many request you want to process before closing the server\ntimeout=5\nmaxalive=15\n\n[HOST:0] #The number must start from 0 to hosts - 1\nname= #Name of the website\nroot= #root path (from http-server folder of absolute path)\nindex= #Default file to open if none is selected\n");
+    fclose(file);
+}
+
+void free_config(Config_server *config){
+    for (int i = 0; i < config->hosts; i++)
+    {
+        free(config->websites[i].index);
+        free(config->websites[i].name);
+        free(config->websites[i].root);
+    }
+    free(config->websites);
+    free(config);
 }
 
 char *find_section(char *start, char *name, char **end){
@@ -124,49 +89,23 @@ int find_field_s(char *start, char *name, char *end, char **dest, int *len){
 
     while ( temp < end && *temp != '\0' && *temp != '#' && *temp != '\n' && *temp != ' ') temp++;
 
-    if ( temp >= end || *temp == '\0') return 0;    
+    if ( temp >= end || *temp == '\0') return 1;    
 
     *len = temp - pos;
 
     *dest = malloc(*len + 1);
 	memset(*dest, '\0', *len + 1);
     strncpy( *dest, pos, *len);
-    return 1;
+    return 0;
 }
 
 int find_field_n(char *start, char *name, char *end, int *dest){
     char *pos = strstr( start, name);
     pos += strlen(name);
 
-    if ( end <= pos ) return 0;
+    if ( end <= pos ) return 1;
 
     *dest = strtol(pos, &end, 10);
 
-    return end == pos ? 0 : 1;
-}
-
-Website *find_website(Config_server *config, char *host, int len){
-	int count = config->hosts;
-	for (int i = 0; i < count; i++)
-	{
-		if ( strncmp(host, config->websites[i].name, len) == 0) return &config->websites[i];
-	}
-	return NULL;
-}
-
-void free_config(Config_server *config){
-    for (int i = 0; i < config->hosts; i++)
-    {
-        free(config->websites[i].index);
-        free(config->websites[i].name);
-        free(config->websites[i].root);
-    }
-    free(config->websites);
-    free(config);
-}
-
-void generate_config_file(){
-    FILE *file = open_file("server.ini","w");
-    fprintf(file,"[CONFIG]\nport=8080  #Listen Port\nphp_port=9000 #Port of the php server\nhosts=1  #Numbers of host\nmaxcycle=0   #How many request you want to process before closing the server\ntimeout=5\nmaxalive=15\n\n[HOST:0] #The number must start from 0 to hosts - 1\nname= #Name of the website\nroot= #root path (from http-server folder of absolute path)\nindex= #Default file to open if none is selected\n");
-    fclose(file);
+    return end == pos ? 1 : 0;
 }
--- src/config.h
+++ src/config.h
@@ -1,9 +1,55 @@
 #ifndef CONFIG_H
     #define CONFIG_H
+	/**
+	 * @file config.h
+	 * @author ROBERT Benjamin
+	 * @brief Process the config file.
+	 * @version 1.0
+	 * @date 2022-04-8
+	 * 
+	 * @see Config_server
+	 * @see Website
+	 * 
+	 * Exemple du fichier config :
+	 * \code{.ini}
+	[CONFIG]
+	port=8080  #Port d'écoute
+	php_port=9000 #Port du serveur fastcgi
+	hosts=3    #Nombre de site
+	maxcycle=0 #Nombre de fois que le serveur va répondre à une requête, 0 si infini
+	timeout=5
+	maxalive=15
+
+	[HOST:0]
+	name=toto0.com
+	root=websites/toto0.com/
+	index=index0.html
+
+	[HOST:1]
+	name=toto1.com
+	root=websites/toto1.com/
+	index=index2.html
+
+	[HOST:2]
+	name=toto2.com
+	root=websites/toto2.com/
+	index=index2.html
+	* \endcode
+	*/
+
+	/* System Includes */
+
+	#include <stdio.h>
+	#include <string.h>
+	#include <unistd.h>
+	#include <stdlib.h>
+
+	/* User Includes */
+
+	#include "utils.h"
 
 	/**
 	 * @brief Contain Basic information about each virtual host.
-	 * 
 	 */
     typedef struct{
         char *name;
@@ -16,7 +62,6 @@
 
 	/**
 	 * @brief Contain all the information needed to start and close the http-server.
-	 * 
 	 */
     typedef struct{
         Website *websites;
@@ -28,14 +73,74 @@
 		int keepMax;
     }Config_server;
 
-
+	/**
+	 * @brief Read the config file `file_name`.
+	 * @see Config_server
+	 * @see Website
+	 * 
+	 * @param file_name 
+	 * @return Return he pointer to the struct with all the config informations or NULL if an error as occured.
+	 */
     Config_server *get_config(char *file_name);
     
-    void free_config(Config_server *config);
+	/**
+	 * @brief Get the current website based on the host.
+	 * 
+	 * @param config The pointer to the current config loaded.
+	 * @param host The 'name' of the website, we want to get.
+	 * @param len The length of the host.
+	 * @return Return the pointer to all information about this website. NULL if not found.
+	 */
+	Website *find_website(Config_server *config, char *host, int len);
 
+	/**
+	 * @brief Generate a default config file.
+	 */
     void generate_config_file();
 
-	Website *find_website(Config_server *config, char *host, int len);
+	/**
+	 * @brief Free the config data structure.
+	 * 
+	 * @param config The pointer to the current config loaded.
+	 */
+    void free_config(Config_server *config);
+
+	/* Static declaration */
+
+	/**
+	 * @brief Find the start of a section.
+	 * 
+	 * @param start The start of the part of the char* where we want to find the field.
+	 * @param name The name of the section.
+	 * @param end The end of the part of the char* where we want to find the field.
+	 * @return Return the address of the section or NULL if the section was not found.
+	 */
+	static char *find_section(char *start, char *name, char **end);
+
+	/**
+	 * @brief Find the a field of type string and copy it into a dynamically allocated char *.
+	 * 
+	 * @param start The start of the section of the char* where we want to find the field.
+	 * @param name The name of the field.
+	 * @param end The end of the section of the char* where we want to find the field.
+	 * @param dest The destination of the dynamically allocated char *;
+	 * @param len The length of the created char*.
+	 * @return Return 0 for success and 1 for failure
+	 */
+	static int find_field_s(char *start, char *name, char *end, char **dest, int *len);
+
+	/**
+	 * @brief Find the a field of type integer and copy it.
+	 * 
+	 * @param start The start of the section of the char* where we want to find the field.
+	 * @param name The name of the field.
+	 * @param end The end of the section of the char* where we want to find the field.
+	 * @param dest The destination of the interger;
+	 * @return Return 0 for success and 1 for failure
+	 */
+	static int find_field_n(char *start, char *name, char *end, int *dest);
+
+
 
 #endif
 
